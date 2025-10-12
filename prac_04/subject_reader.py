c9c404c9f098f46ad5b69d58ffde14055d6b6e95
TOBI <henry.song778@gmail.com>
2025-10-12 21:35:18 +0800

    Added read data file and display subject details Added data from file formatted like: subject , lecturer, number of students Added display subject details neatly

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,28 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+  
+    subject_records = load_data(FILENAME)
+    display_subject_details(subject_records)
 
 
 def load_data(filename=FILENAME):
-    """Read data from file formatted like: subject,lecturer,number of students."""
+
+    subject_records = []
     input_file = open(filename)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])
+        subject_records.append(parts)
     input_file.close()
+    return subject_records
+
+
+def display_subject_details(subject_records):
+
+    for subject_code, lecturer, num_students in subject_records:
+        print(f"{subject_code} is taught by {lecturer} and has {num_students} students")
 
 
 main()
