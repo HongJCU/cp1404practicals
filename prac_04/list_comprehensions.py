ca8ee45e0333af5c51ab12bb8f539e6173282120
TOBI <henry.song778@gmail.com>
2025-10-12 21:45:49 +0800

    Added filtereing names that start with A Added join string methods

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -6,28 +6,22 @@ List comprehensions
 names = ["Bob", "Angel", "Jimi", "Alan", "Ada"]
 full_names = ["Bob Martin", "Angel Harlem", "Jimi Hendrix", "Alan Turing", "Ada Lovelace"]
 
-# for loop that creates a new list containing the first letter of each name
 first_initials = []
 for name in names:
     first_initials.append(name[0])
 print(first_initials)
 
-# list comprehension that does the same thing as the loop above
 first_initials = [name[0] for name in names]
 print(first_initials)
 
-# list comprehension that creates a list containing the initials
 full_initials = [name.split()[0][0] + name.split()[1][0] for name in full_names]
 print(full_initials)
 
-# filtering names that start with A
 a_names = [name for name in names if name.startswith('A')]
 print(a_names)
 
-# join string method to create a single string from the names
 print(" ".join(sorted(names)))
 
-# TODOs completed below 👇
 lowercase_full_names = [name.lower() for name in full_names]
 print(lowercase_full_names)
 
