ad13b82ee9387508feb6332ac78fc8c69736a9e7
TOBI <henry.song778@gmail.com>
2025-10-12 21:40:02 +0800

    Created a listed_exercise python file, added an input to ask user to put five number and display out each number

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,18 @@
+
+numbers = []
+
+
+for i in range(5):
+    num = float(input("Number: "))  
+    numbers.append(num)
+
+
+for num in numbers:
+    print(f"Number: {num}")
+
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
