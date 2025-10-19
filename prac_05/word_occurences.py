12eff168efc6bf80a3781968c9ef889a483921b5
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-10-19 19:31:51 +0800

    Addedd a sorted output by alphabetical and find the longest word and print everything aligned column

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -9,5 +9,22 @@ text = input("Text: ")
 words = text.split()
 print(words)
 
+counts ={}
+for word in words:
+    if word in counts:
+        counts[word] += 1
+    else:
+        counts[word] = 1
 
+print(counts)
+
+#Get a sorted output by alphabetically by words
+sorted_words = sorted(counts.keys())
+
+# Find the longest word and print alligned column
+max_width = max(len(w) for w in sorted_words)
+
+for w in sorted_words:
+    print(f"{w:{max_width}} : {counts[w]}")
+print(sorted_words)
 
