33ff8778e24c6c0b6f794bf6218b2f1ff2bb7e89
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-10-19 19:55:25 +0800

    #Create a variable to transform from email to name and a loop to split the name into two parts , also asking user to confirm the name or not

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -11,5 +11,19 @@ def get_name_from_email(email):
     name = "".join(parts).title()
     return name
 
+#Create a variable to transform from email to name
+email_to_name = {}
+
+# A loop to ask user to input the email address and confirm whethere is it correct or not 
+email = input("Email: ")
+while email != "":
+    email_to_name[get_name_from_email(email)] = email
+    name = get_name_from_email(email)
+    confirmation = input(f"Is your name {name}? (Y/n) ").strip().lower()
+    if confirmation not in("","y"):
+        name = input("Name: ").title()
+    email_to_name[email] = name
+    email = input("Email: ")
+
 emails = input("Enter emails separated by space: ")
 print(emails)
