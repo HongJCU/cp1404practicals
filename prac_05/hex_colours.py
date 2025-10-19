3f3a9d0d0c10b3160ad07bfc398b2b780a39f122
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-10-19 18:52:42 +0800

    Created a file called hex_color and adding listed as same as state names but replacing names and shortcut name into codes

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,9 @@
+"""
+CP1404/CP5632 Practical
+Hex Color in dictionary
+Reformatted to follow PEP 8 conventions
+"""
+# Adding color listing
+COLOR_TO_CODE = {"Absolute Zero": "#0048ba","Acid Green": "#b0bf1a","Alice Blue": "#f0f8ff","Alizarin Crimson": "#e32636",
+                 "Amethyst": "#9966cc","Amber": "#ffbf00","Apricot": "#fbceb1","Baby Pink": "#f4c2c2",
+                 "Banana Yellow": "#ffe135","Bitter Lime": "#bfff00"}
