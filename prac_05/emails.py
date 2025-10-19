40f69e6e0cf16fee5c68b7e42245b30a8efffe68
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-10-19 19:50:41 +0800

    Added a function to split the email between . and @ to get the first and last name

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,5 +4,12 @@ Estimate: 20 minutes
 Actual:   32 minutes
 """
 
+#Added a function to split the email between . and @ to get the first and last name
+def get_name_from_email(email):
+    prefix = email.split("@")[0]
+    parts =  prefix.split('.')
+    name = "".join(parts).title()
+    return name
+
 emails = input("Enter emails separated by space: ")
 print(emails)
