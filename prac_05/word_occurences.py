29c91fbee0b9eb7577b35f6582c53af90267e354
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-10-19 19:17:16 +0800

    Added split function to easier to count the words

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -3,6 +3,11 @@ Word Occurrences
 Estimate: 30 minutes
 Actual:   32 minutes
 """
+
+#Adding the function request user to type the text, program will spit and count it
 text = input("Text: ")
 words = text.split()
 print(words)
+
+
+
