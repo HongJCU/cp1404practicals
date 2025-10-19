710f232225713148fd8f2885516962c4d44280c9
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-10-19 20:56:43 +0800

    Created a function to display result as formated ( count , and show )

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,7 +1,7 @@
 """
 Wimbledon.py
 Estimate: 60 minutes
-Actual:   40 minutes
+Actual:   50 minutes
 """
 
 # Add a funciton to read csv file
@@ -25,3 +25,18 @@ def count_champions(data):
 def get_countries(data):
     countries = {row[1] for row in data}
     return sorted(countries)
+
+# Created a function to display the result as formatted
+def main():
+    filename = "wimbledon.csv"
+    data=read_csv_file(filename)
+    champion_to_count = count_champions(data)
+    countries = get_countries(data)
+    print("Wimbledon Champions:")
+    for champion, wins in sorted(champion_to_count.items()):
+        print(f"{champion:20} {wins}", sep="")
+
+    print(f"There {len(countries)} countries have won Wimbledon: ", ", ".join(countries))
+
+
+main()
