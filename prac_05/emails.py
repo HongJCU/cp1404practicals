f2b1b31deb48a8767f176059d869bbdf4cee574c
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-10-19 20:02:21 +0800

    Remodified the loop to ask user to input the email address, confirm the name and created a definination so it could display into the main

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,24 +6,27 @@ Actual:   32 minutes
 
 #Added a function to split the email between . and @ to get the first and last name
 def get_name_from_email(email):
-    prefix = email.split("@")[0]
-    parts =  prefix.split('.')
-    name = "".join(parts).title()
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
     return name
 
-#Create a variable to transform from email to name
-email_to_name = {}
+def main():
 
-# A loop to ask user to input the email address and confirm whethere is it correct or not 
-email = input("Email: ")
-while email != "":
-    email_to_name[get_name_from_email(email)] = email
-    name = get_name_from_email(email)
-    confirmation = input(f"Is your name {name}? (Y/n) ").strip().lower()
-    if confirmation not in("","y"):
-        name = input("Name: ").title()
-    email_to_name[email] = name
+    #Create a variable to transform from email to name and store the information
+    email_to_name = {}
+
+    # A loop to ask user to input the email address and confirm whethere is it correct or not
     email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ").strip().lower()
+        if confirmation not in("","y"):
+            name = input("Name: ").title()
+        email_to_name[email] = name
+        email = input("Email: ")
 
-emails = input("Enter emails separated by space: ")
-print(emails)
+    for email,name in email_to_name.items():
+        print(f"{name}: {email}")
+
+main()
