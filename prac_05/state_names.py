84fa6478cfc634ef3f41622d55085529eef489fd
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-10-19 18:39:02 +0800

    Change the program so lowercase also works

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,13 +9,14 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 
 # Change the program so lowercase inputs also works
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+
 
 
 
