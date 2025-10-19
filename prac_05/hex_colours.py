a6e8dda8f534a6dd8fdc888f272bcf9dc3ec579c
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-10-19 18:53:25 +0800

    Adding loop to list out the color name and code linedup

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -7,3 +7,7 @@ Reformatted to follow PEP 8 conventions
 COLOR_TO_CODE = {"Absolute Zero": "#0048ba","Acid Green": "#b0bf1a","Alice Blue": "#f0f8ff","Alizarin Crimson": "#e32636",
                  "Amethyst": "#9966cc","Amber": "#ffbf00","Apricot": "#fbceb1","Baby Pink": "#f4c2c2",
                  "Banana Yellow": "#ffe135","Bitter Lime": "#bfff00"}
+
+# Print all color names and their codes neatly lined up
+for color, code in COLOR_TO_CODE.items():
+    print(f"{color:20} is {code}")
