61f72e688d5dabed92dd6f0e79e2821cdda7612b
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-11-02 20:08:12 +0800

    + Added __str__(self) converting the informations into a string + Added is_dynamic to check if user typed the name or not

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -10,10 +10,9 @@ class ProgrammingLanguage:
         self.year = year
 
 def is_dynamtic(self):
-    # Check if the user inpput the languge name is dynamically typed or not 
+    # Check if the user inpput the languge name is dynamically typed or not
     return self.typing.lower() == "dynamic"
 
 def __str__(self):
 # Pulling the value from four different attributes of the object (self.name, self.typing, self.reflection, and self.year)
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
-
