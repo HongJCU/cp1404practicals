ebd2ffbb27f102118d5e1aeba61b84a29e8bc361
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-11-02 21:38:50 +0800

    Comment time estimate for used_cars and cars

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -3,7 +3,12 @@ CP1404/CP5632 Practical - Client code to use the Car class.
 Note that the import has a folder (module) in it.
 This is why we name our folders with no spaces or capitals, as valid module names.
 """
-
+"""CP1404/CP5632 Practical - Car class example."""
+"""
+car
+Estimate: 30 minutes
+Actual:   38 minutes
+"""
 from prac_06.car import Car
 
 
