09c82aa8d9237f49a5cf2cd5dbd130bfb91d410b
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-11-02 19:58:05 +0800

    Initialise a programminglanguage instance.

---
+++ prac_06/programming_language.py
@@ -0,0 +1,9 @@
+"""CP1404/CP5632 Practical - Programming Language class."""
+
+class ProgrammingLanguage:
+
+    def __init__(self,name, typing, reflection , year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
