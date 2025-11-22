8cc4cfb71532fd5718521d7840674ee28c20b45c
HongJCU <khanhhong.song@my.jcu.edu.au>
2025-11-22 14:04:01 +0800

    + Added a new attribute for the silver_taxi -> fanciness and multiply taxi.price_per_km
    +Added extra charge for taxicharge 4.50
    
    +Added a method to calculate the fare

---
+++ prac_06/SilverServiceTaxi.py
@@ -0,0 +1,17 @@
+"""CP1404/CP5632 Practical - SilverServiceTaxi class"""
+from prac_09.taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi with scaled price per km."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * fanciness  # scale base price
+
+    def get_fare(self):
+        """Calculate fare including flagfall."""
+        return super().get_fare() + self.flagfall
+
+
